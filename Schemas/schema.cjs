680f9fe64f6d4de823a86a91303d4b39a9554542
iamapurba2003 <husky-on-line@hotmail.com>
2022-07-08 18:26:32 +0530

    Track Completed Added Bi-directional Connections between CompanyType & UserType. Also, discovered a Way of Using fields as a Callback Arrow Function so as to ignore Circular Calls in Js & GraphQl. Also, defined new queries with parameters. Added Mutations like EditUser, DeleteUser, AddUser etc. with different methodologies. Finally, added Some Changes so as to better Work it Out.

--- Schemas/schema.cjs
+++ Schemas/schema.cjs
@@ -1,5 +1,12 @@
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLSchema,
+	GraphQLList,
+	GraphQLNonNull,
+} = graphql;
 const axios = require("axios");
 
 // List of HardCoded Users made to Test the GraphQl Server at the First Go. Now using a Json-Server instance in it's place
@@ -10,16 +17,24 @@ const axios = require("axios");
 
 const CompanyType = new GraphQLObjectType({
 	name: "Company",
-	fields: {
+	fields: () => ({
 		id: { type: GraphQLString },
 		name: { type: GraphQLString },
 		description: { type: GraphQLString },
-	},
+		users: {
+			type: new GraphQLList(UserType),
+			resolve(parentValue, args) {
+				return axios
+					.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+					.then((response) => response.data);
+			},
+		},
+	}),
 });
 
 const UserType = new GraphQLObjectType({
 	name: "User",
-	fields: {
+	fields: () => ({
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt },
@@ -28,6 +43,48 @@ const UserType = new GraphQLObjectType({
 			resolve(parentValue, args) {
 				return axios
 					.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+					.then((response) => response.data);
+			},
+		},
+	}),
+});
+
+const mutation = new GraphQLObjectType({
+	name: "Mutation",
+	fields: {
+		addUser: {
+			type: UserType,
+			args: {
+				firstName: { type: new GraphQLNonNull(GraphQLString) },
+				age: { type: new GraphQLNonNull(GraphQLInt) },
+				companyId: { type: GraphQLString },
+			},
+			resolve(parentValue, { firstName, age }) {
+				return axios
+					.post("http://localhost:3000/users", { firstName, age })
+					.then((response) => response.data);
+			},
+		},
+		deleteUser: {
+			type: UserType,
+			args: { id: { type: new GraphQLNonNull(GraphQLString) } },
+			resolve(parentValue, args) {
+				return axios
+					.delete(`http://localhost:3000/users/${args.id}`)
+					.then((response) => response.data);
+			},
+		},
+		editUser: {
+			type: UserType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString) },
+				firstName: { type: GraphQLString },
+				age: { type: GraphQLInt },
+				companyId: { type: GraphQLString },
+			},
+			resolve(parentValue, args) {
+				return axios
+					.patch(`http://localhost:3000/users/${args.id}`, args)
 					.then((response) => response.data);
 			},
 		},
@@ -60,4 +117,5 @@ const RootQuery = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
 	query: RootQuery,
+	mutation: mutation,
 });
